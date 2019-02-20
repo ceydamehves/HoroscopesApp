98326cbb885022aa509f58b8074220d129c080f7
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-02-20 23:19:23 +0300

    mainscreen added.

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,15 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
+import MainScreen from '../screens/MainScreen.js';
 import Horoscopes from '../screens/Horoscopes.js';
 import HoroscopeComment from '../screens/HoroscopeComment.js';
 import HoroscopePages from '../screens/HoroscopePages.js';
 
+
 const AppNavigator = createStackNavigator (
   {
+    MainScreen: {
+      screen: MainScreen,
+    },
     Horoscopes: {
       screen: Horoscopes,
     },
