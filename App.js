e13af8584571c77e9466e54c7401c778ad222374
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-02-26 17:43:47 +0300

    status bar corrected

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {StatusBar,View} from 'react-native';
 import AppContainer from './navigation/AppNavigator.js';
 
 export default class App extends React.Component {
   render () {
-    return <AppContainer />;
+    return (
+    <View style={{flex:1}}>
+    <StatusBar translucent={true} backgroundColor="rgba(171, 184, 206,0.05)"/>
+    <AppContainer/>
+    </View>
+    );
   }
 }
